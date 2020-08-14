9c402a384d61d7d0542fedabd80dd30091787208
rangjin <46679581+rangjin@users.noreply.github.com>
2020-08-14 14:22:50 +0900

    회원가입

--- isteam/groupware/views.py
+++ isteam/groupware/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from .forms import RegisterForm
+
 
 # Create your views here.
+
+class Register(FormView):
+    form_class = RegisterForm
+    success_url = '/../'
