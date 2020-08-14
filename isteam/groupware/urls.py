b2a711bd15b6954e5bc6821ffcca799d8344eb9f
rangjin <46679581+rangjin@users.noreply.github.com>
2020-08-15 02:34:10 +0900

    이메일 인증

--- isteam/groupware/urls.py
+++ isteam/groupware/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.Register.as_view(template_name='register.html'), name='register')
+    path('register/', views.register, name='register'),
+    path('activate/<str:uidb64>/<str:token>', views.activate, name='activate'),
 ]
