07de9faff3011718e8ed2c2f21f3d92d9f47451d
rangjin <46679581+rangjin@users.noreply.github.com>
2020-08-14 14:22:03 +0900

    User model 수정

--- isteam/groupware/models.py
+++ isteam/groupware/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     student_id = models.CharField(max_length=8)
 
-    name = models.CharField(max_length=10)
-
     email = models.CharField(max_length=30)
+
+    password = models.CharField(max_length=128)
 
     active = models.BooleanField(default=True)
 
     def __str__(self):
-        return f'Student id: {self.student_id}\nName: {self.name}'
+        return f'Student id: {self.student_id}\nName: {self.username}'
