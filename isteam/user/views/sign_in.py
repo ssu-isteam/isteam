8fa98b9460322fd9226037124266e6d133635625
5d-jh <jeonghyun0126@outlook.com>
2020-08-17 00:13:59 +0900

    로그인 프로세스 수정

--- isteam/user/views/sign_in.py
+++ isteam/user/views/sign_in.py
@@ -1,5 +1,5 @@
 from django.views.generic import FormView
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.hashers import check_password
 
 from user.forms.sign_in import SignInForm
@@ -13,13 +13,11 @@ class SignIn(FormView):
 
     def form_valid(self, form):
         nickname = form.data['nickname']
-        password = check_password(form.data['password'])
-        member = authenticate(username='ddd', password=password)
+        password = form.data['password']
+        member = authenticate(username=nickname, password=password)
 
         if member is not None:
-            self.request.session.set_test_cookie()
-            self.request.session['hello'] = 'world'
-            self.request.session.save()
+            login(self.request, member)
             return super().form_valid(form)
         else:
             return super().form_invalid(form)
