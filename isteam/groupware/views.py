8c60e28cfb988798066b68d7da054f1f34b4e381
5d-jh <jeonghyun0126@outlook.com>
2020-08-17 22:32:15 +0900

    View 수정

--- isteam/groupware/views.py
+++ isteam/groupware/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from user.models import Member
 
 
-class GroupwareMainPage(DetailView):
+class GroupwareMainPage(LoginRequiredMixin, DetailView):
+    login_url = '/user/signin/'
+
     model = Member
 
     context_object_name = 'member_info'
 
-    template_name = 'index.html'
+    template_name = 'groupware.html'
+
+    def get_object(self):
+        return self.request.user
