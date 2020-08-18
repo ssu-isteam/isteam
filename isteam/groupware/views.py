3f5fc59b6f71fee3d388052a60a2ad4922cbdb3c
이정현 <jeonghyun0126@outlook.com>
2020-08-18 17:56:11 +0900

    Feature/groupware (#7)
    * View 수정
    
    * View 수정
    
    * Tab 수정

--- isteam/groupware/views.py
+++ isteam/groupware/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from user.models import Member
 
 
-class GroupwareMainPage(DetailView):
+class GroupwareMainPage(LoginRequiredMixin, DetailView):
+    login_url = '/user/signin/'
+
     model = Member
 
     context_object_name = 'member_info'
 
-    template_name = 'index.html'
+    template_name = 'groupware.html'
+
+    def get_object(self):
+        return self.request.user
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+
+        tabs = {
+            'activities': '활동',
+            'users': '회원명단',
+            'settings': '설정'
+        }
+        selected = self.request.GET.get('tab')
+
+        if selected is not None and selected in tabs.keys():
+            context['selected'] = selected
+        else:
+            context['selected'] = list(tabs.keys())[0]
+        
+        context['tabs'] = tabs.items()
+
+        return context
