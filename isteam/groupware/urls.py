caca2874647d906bed3d01c1a2e6402dc6e4922e
rangjin <46679581+rangjin@users.noreply.github.com>
2020-08-22 01:03:47 +0900

    멤버 조회 추가

--- isteam/groupware/urls.py
+++ isteam/groupware/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from groupware.views.accounts import AccountBookListView
+from groupware.views.users import UserListView
 
 
 urlpatterns = [
-    path('accounts/', AccountBookListView.as_view(), name='groupware_accounts')
+    path('accounts/', AccountBookListView.as_view(), name='groupware_accounts'),
+    path('users/', UserListView.as_view(), name='groupware_users'),
 ]
---
+++ isteam/groupware/views/users.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from user.models import Member
+from groupware.views.base import GroupwareView
+
+
+class UserListView(GroupwareView, ListView):
+    model = Member
+
+    template_name = 'users.html'
+
+    context_object_name = 'Users'
+
+    tab_name = 'users'
+
+    def get_queryset(self):
+        new_context = Member.objects.filter(email_verified=True).order_by('student_id')
+        return new_context
+
+    def get_context_data(self):
+        context = super().get_context_data()
+
+        context['tabs'] = self.tab_items
+        context['selected'] = self.select_tab(self.tab_name)
+
+        return context
