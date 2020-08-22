89153eea91a3304cf97fc63fc3d03702447f860c
rangjin <46679581+rangjin@users.noreply.github.com>
2020-08-22 18:45:19 +0900

    로그인한 사용자 정보 추가

--- isteam/groupware/views/users.py
+++ isteam/groupware/views/users.py
@@ -24,5 +24,6 @@ class UserListView(GroupwareView, ListView):
 
         context['tabs'] = self.tab_items
         context['selected'] = self.select_tab(self.tab_name)
+        context['member_info'] = self.request.user
 
         return context
