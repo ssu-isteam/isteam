e964cd668741c1d63be3c3777a6643e4272a01ec
5d-jh <jeonghyun0126@outlook.com>
2020-08-26 16:38:18 +0900

    Change key

--- scripts/on-travis.sh
+++ scripts/on-travis.sh
@@ -3,4 +3,4 @@
 openssl aes-256-cbc -K $encrypted_f217180e22ee_key -iv $encrypted_f217180e22ee_iv -in id_rsa.enc -out id_rsa -d
 openssl aes-256-cbc -K $encrypted_f182de71524d_key -iv $encrypted_f182de71524d_iv -in .prod.env.enc -out .prod.env -d
 
-ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=no -i '../id_rsa' $SSH_HOST < scripts/on-isteam.sh
+ssh -o StrictHostKeyChecking=no -i './id_rsa' $SSH_HOST < scripts/on-isteam.sh
