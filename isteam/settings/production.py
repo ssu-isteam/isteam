b11c7f91dc2a77c154bab8619e2c57e9be7b9f7f
5d-jh <jeonghyun0126@outlook.com>
2020-08-26 21:22:29 +0900

    Add charset

--- isteam/settings/production.py
+++ isteam/settings/production.py
@@ -45,6 +45,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'charset': 'utf8',
         },
     },
 ]
