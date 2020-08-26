9265a978ec3076808dbefe0372e0bf91464e48de
5d-jh <jeonghyun0126@outlook.com>
2020-08-26 22:51:17 +0900

    Member model 수정

--- user/models.py
+++ user/models.py
@@ -7,12 +7,12 @@ class Member(AbstractUser):
     student_id = models.CharField(max_length=8)
 
     # @soongsil.ac.kr로 끝나는 이메일
-    email = models.CharField(max_length=30)
+    email = models.CharField(max_length=30, null=True, blank=False)
 
-    password = models.CharField(max_length=128)
+    password = models.CharField(max_length=128, null=True, blank=False)
 
     # 이메일 인증 여부
     email_verified = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'Student id: {self.student_id}\nName: {self.username}'
+        return f'{self.student_id} {self.username}'
