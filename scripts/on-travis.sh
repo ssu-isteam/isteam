db3fa90242436539f7015e9bf7bc90ccf8ec3cb8
5d-jh <jeonghyun0126@outlook.com>
2020-08-26 16:23:06 +0900

    Fix path

--- scripts/on-travis.sh
+++ scripts/on-travis.sh
@@ -3,4 +3,4 @@
 openssl aes-256-cbc -K $encrypted_f217180e22ee_key -iv $encrypted_f217180e22ee_iv -in id_rsa.enc -out id_rsa -d
 openssl aes-256-cbc -K $encrypted_f182de71524d_key -iv $encrypted_f182de71524d_iv -in .prod.env.enc -out .prod.env -d
 
-ssh $SSH_HOST -i '../id_rsa' < ./on-isteam.sh
+ssh $SSH_HOST -i '../id_rsa' < scripts/on-isteam.sh
