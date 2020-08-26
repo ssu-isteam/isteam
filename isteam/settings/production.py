c58e7da326c89e651181a430cd0dd19ab0640b27
5d-jh <jeonghyun0126@outlook.com>
2020-08-26 19:30:25 +0900

    Fix settings

--- isteam/settings/production.py
+++ isteam/settings/production.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['app']
+ALLOWED_HOSTS = ['isteam.dev', 'localhost']
 
 TEMPLATES = [
     {
