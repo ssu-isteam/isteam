1a0b223a902dcca980520e0415ee063468ae7a4e
5d-jh <jeonghyun0126@outlook.com>
2020-08-26 17:22:15 +0900

    Add password

--- scripts/on-travis.sh
+++ scripts/on-travis.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-openssl aes-256-cbc -K $encrypted_f217180e22ee_key -iv $encrypted_f217180e22ee_iv -in id_rsa.enc -out id_rsa -d
 openssl aes-256-cbc -K $encrypted_f182de71524d_key -iv $encrypted_f182de71524d_iv -in .prod.env.enc -out .prod.env -d
-cp id_rsa ~/.ssh
-chmod 400 ~/.ssh/id_rsa
+
+sudo apt update
+sudo apt install sshpass
 
 ssh-add
-ssh $SSH_HOST < scripts/on-isteam.sh
+sshpass -p "$SSH_PASS" ssh -o StrictHostKeyChecking=no $SSH_HOST < scripts/on-isteam.sh
