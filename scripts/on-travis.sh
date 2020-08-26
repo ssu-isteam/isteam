3da93d4076571c8da49d5be4afeda7f97eb0a58d
5d-jh <jeonghyun0126@outlook.com>
2020-08-26 16:58:28 +0900

    Fix cp

--- scripts/on-travis.sh
+++ scripts/on-travis.sh
@@ -2,7 +2,8 @@
 
 openssl aes-256-cbc -K $encrypted_f217180e22ee_key -iv $encrypted_f217180e22ee_iv -in id_rsa.enc -out id_rsa -d
 openssl aes-256-cbc -K $encrypted_f182de71524d_key -iv $encrypted_f182de71524d_iv -in .prod.env.enc -out .prod.env -d
-chmod 400 ./id_rsa
+cp id_rsa ~/.ssh
+chmod 400 ~/.ssh/id_rsa
 
 ssh-add
 ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=no -i './id_rsa' $SSH_HOST < scripts/on-isteam.sh
