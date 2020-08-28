9fd28f36fae0076e0d148cf3488a8953145762cd
5d-jh <jeonghyun0126@outlook.com>
2020-08-28 20:55:18 +0900

    Template 이동

--- recruit/views.py
+++ recruit/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.views.generic import FormView
 from .forms import RecruitForm
 
-# Create your views here.
-
 
 class RecruitView(FormView):
     form_class = RecruitForm
-    template_name = 'recruit.html'
+
+    template_name = 'recruit/recruit.html'
+
     success_url = '/'
