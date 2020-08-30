4e7ffa142a7ec850b9d1996e51d4756ece867e88
5d-jh <jeonghyun0126@outlook.com>
2020-08-30 21:50:12 +0900

    Layout 위치 변경

--- isteam/settings/base.py
+++ isteam/settings/base.py
@@ -52,6 +52,24 @@ WEBPACK_LOADER = {
     }
 }
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, '../templates')
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
--- isteam/settings/ci.py
+++ isteam/settings/ci.py
@@ -29,23 +29,6 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
