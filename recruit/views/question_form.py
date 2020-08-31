e8d9d896acb536da60c1fff21fa8d09b000bcb4a
5d-jh <jeonghyun0126@outlook.com>
2020-08-31 18:51:01 +0900

    Cookie 검증 추가

--- recruit/views/question_form.py
+++ recruit/views/question_form.py
@@ -1,7 +1,11 @@
+import json
+
 from django.views.generic import FormView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from recruit.forms.question import QuestionForm
-from recruit.models import Recruitment, Question
+from recruit.models import Recruitment, Question, Applicant
 
 
 class QuestionFormView(FormView):
@@ -11,5 +15,22 @@ class QuestionFormView(FormView):
 
     success_url = '/'
 
-    def get_context_data(self, **kwargs):
-        pass
+    applicant_profile = {}
+
+    def form_valid(self, form):
+        return super().form_valid(form)
+
+    def get(self, *args, **kwargs):
+        try:
+            profile = self.request.COOKIES.get('profile')
+            profile = json.loads(profile)
+
+            for key in ('username', 'student_id', 'email', 'phone_number'):
+                if type(profile[key]) is not str:
+                    raise Exception()
+                else:
+                    self.applicant_profile[key] = profile[key]
+            
+            return super().get(*args, **kwargs)
+        except:
+            return HttpResponseRedirect(reverse('recruit'))
