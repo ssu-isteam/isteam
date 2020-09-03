be5e9e30f995840cc892cb39e650cc4be74b72f7
5d-jh <jeonghyun0126@outlook.com>
2020-09-03 23:37:20 +0900

    CSS 깨짐 수정

--- isteam/settings/base.py
+++ isteam/settings/base.py
@@ -41,7 +41,9 @@ WSGI_APPLICATION = 'isteam.wsgi.application'
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, '../static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, '../static')
+]
 
 WEBPACK_LOADER = {
     'DEFAULT': {
