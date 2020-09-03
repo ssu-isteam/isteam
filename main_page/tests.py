a4e26624fb224a9545be3752eeeb1056ee849263
5d-jh <jeonghyun0126@outlook.com>
2020-09-03 16:27:42 +0900

    main_page 테스트 코드 추가

--- main_page/tests.py
+++ main_page/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class MainPageViewTest(TestCase):
+    def test_get(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
