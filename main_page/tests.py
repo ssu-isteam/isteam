c35f4ced751810f26f91b14d430443f15730bb4b
5d-jh <jeonghyun0126@outlook.com>
2020-09-03 16:34:46 +0900

    Recruit 테스트 코드 추가

--- main_page/tests.py
+++ main_page/tests.py
@@ -2,6 +2,6 @@ from django.test import TestCase
 
 
 class MainPageViewTest(TestCase):
-    def test_get(self):
+    def test_get_success(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
