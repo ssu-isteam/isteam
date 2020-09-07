97e64fbdbcd165cd1bd271ce3b4bd630747abf39
5d-jh <jeonghyun0126@outlook.com>
2020-09-07 11:56:50 +0900

    Model 가독성 개선

--- recruit/admin.py
+++ recruit/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from recruit.models import Answer, Question, Recruitment
+from recruit.models import Answer, Question, Recruitment, Applicant
 
 
 admin.site.register(Answer)
 admin.site.register(Question)
 admin.site.register(Recruitment)
+admin.site.register(Applicant)
