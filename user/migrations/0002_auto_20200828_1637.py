5d9052df5535204d9d87563e109456c4b0c6e9e6
5d-jh <jeonghyun0126@outlook.com>
2020-09-07 13:46:48 +0900

    Migration 수정

--- user/migrations/0002_auto_20200828_1637.py
+++ user/migrations/0002_auto_20200828_1637.py
@@ -3,6 +3,50 @@
 from django.db import migrations, models
 
 
+def forward(apps, schema_editor):
+    Group = apps.get_model('auth', 'Group')
+    Permission = apps.get_model('auth', 'Permission')
+    db_alias = schema_editor.connection.alias
+
+    junior_perm_codenames = [
+        'view_member',
+        'view_accountbook',
+        'view_session',
+        'view_activity',
+        'add_session',
+        'change_session',
+        'delete_session',
+        'view_session'
+    ]
+    junior_perms = Permission.objects.using(db_alias).filter(codename__in=junior_perm_codenames)
+    
+    senior_perm_codenames = junior_perm_codenames + [
+        'add_activity',
+        'change_activity',
+        'delete_activity'
+    ]
+    senior_perms = Permission.objects.using(db_alias).filter(codename__in=senior_perm_codenames)
+
+    manager_perms = Permission.objects.using(db_alias).all()
+
+    junior_group, created = Group.objects.using(db_alias).update_or_create(name='junior')
+    junior_group.permissions.set(junior_perms)
+
+    senior_group, created = Group.objects.using(db_alias).update_or_create(name='senior')
+    senior_group.permissions.set(senior_perms)
+
+    manager_group, created = Group.objects.using(db_alias).update_or_create(name='manager')
+    manager_group.permissions.set(manager_perms)
+
+
+def backward(apps, schema_editor):
+    Group = apps.get_model('auth', 'Group')
+    Permission = apps.get_model('auth', 'Permission')
+
+    Group.objects.using(db_alias).all().delete()
+    Permission.objects.using(db_alias).all().delete()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -15,4 +59,5 @@ class Migration(migrations.Migration):
             name='email',
             field=models.CharField(default='', max_length=30),
         ),
+        migrations.RunPython(forward, backward)
     ]
