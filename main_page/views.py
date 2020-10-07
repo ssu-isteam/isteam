e0b05eb3f346a9535ce81e7869db4c16741209dc
5d-jh <jeonghyun0126@outlook.com>
2020-10-07 17:55:35 +0900

    Footer 수정

--- main_page/views.py
+++ main_page/views.py
@@ -10,6 +10,8 @@ class Index(ListView):
 
     context_object_name = 'activities'
 
+    template_name = 'main_page/index.html'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context[self.context_object_name] = list(context[self.context_object_name])[:3]
