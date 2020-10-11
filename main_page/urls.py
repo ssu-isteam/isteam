f093a6b5f32981dee00f9ca3e9f2fa0628c55403
이정현 <jeonghyun0126@outlook.com>
2020-10-11 19:01:52 +0900

    Feature/view refinement (#67)
    * Layout 분리
    
    * Footer 수정
    
    * Form text 디자인 변경
    
    * 아이콘 추가
    
    * HTML lazy loading 추가

--- main_page/urls.py
+++ main_page/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Index.as_view(template_name='index.html'), name='index')
+    path('', views.Index.as_view(), name='index')
 ]
