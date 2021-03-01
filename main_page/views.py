eab0df9a2f53479ad597d382a7bee7c2de3ffb62
5d-jh <jeonghyun0126@outlook.com>
2021-03-01 22:27:41 +0900

    메인페이지에서 부원모집이 가능한지 확인하는 로직 간소화

--- main_page/views.py
+++ main_page/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
 from django.views.generic import ListView
-from django.urls import reverse
 
 from groupware.models import Activity, Session
+from recruit.functions import is_recruitment_available
 
 
 class Index(ListView):
@@ -16,12 +15,7 @@ class Index(ListView):
         context = super().get_context_data(**kwargs)
         context[self.context_object_name] = list(context[self.context_object_name])[:3]
         context[self.context_object_name].reverse()
-
-        try:
-            context['recruit_url'] = reverse('recruit')
-            context['recruit_available'] = True
-        except:
-            context['recruit_available'] = False
+        context['recruit_available'] = is_recruitment_available()
 
         for activity in context[self.context_object_name]:
             sessions = list(Session.objects.filter(activity=activity.id))
