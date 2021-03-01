446d988988e8db90c5df1c9bd6a719ef63dd613e
5d-jh <jeonghyun0126@outlook.com>
2021-03-01 22:46:23 +0900

    recruit_url 추가

--- main_page/views.py
+++ main_page/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView
+from django.urls import reverse
 
 from groupware.models import Activity, Session
 from recruit.functions import is_recruitment_available
@@ -15,7 +16,9 @@ class Index(ListView):
         context = super().get_context_data(**kwargs)
         context[self.context_object_name] = list(context[self.context_object_name])[:3]
         context[self.context_object_name].reverse()
+
         context['recruit_available'] = is_recruitment_available()
+        context['recruit_url'] = reverse('recruit')
 
         for activity in context[self.context_object_name]:
             sessions = list(Session.objects.filter(activity=activity.id))
