68b7fdf421f67ceff5aef9c963ef6698f3652189
이정현 <jeonghyun0126@outlook.com>
2021-03-05 13:34:08 +0900

    지원자와 지원 서류가 매칭되지 않는 문제 수정 (#82)
    * 개발 환경에서는 캡챠가 작동하지 않도록 수정
    
    * 개발 환경에서 charset 옵션 추가
    
    * 테스트 추가
    
    * QuestionFormView 상태에 지원자를 저장하지 않도록 수정
    
    * CI 환경변수 설정
    
    * APP_ENV 환경변수를 요구하는 값에 맞게 수정

--- utils/recaptcha.py
+++ utils/recaptcha.py
@@ -8,6 +8,9 @@ GOOGLE_CAPTCHA_API_URL = 'https://www.google.com/recaptcha/api/siteverify'
 
 
 def validate_captcha(g_recaptcha_response: str) -> HttpResponseBadRequest:
+    if config('APP_ENV') == 'development':
+        return None
+
     res = requests.post(
         GOOGLE_CAPTCHA_API_URL,
         data={
